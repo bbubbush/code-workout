bce049b4734f72bf0fd1d122e7328dd120f4e386
bbubbush <bbubbush@gmail.com>
2022-05-04 09:52:14 +0900

    feat: modified X589 code

--- src/com/bbubbush/tistory/X158.java
+++ src/com/bbubbush/tistory/X158.java
@@ -4,19 +4,20 @@ public class X158 {
   public static void main(String[] args) {
     int bunnies;
     bunnies = 3;
-    System.out.printf("[bunnies == %s]\n%s\n", bunnies, bunnyEars2(bunnies));
+    System.out.printf("[bunnies = %d]\n", bunnies);
+    System.out.println(bunnyEars2(bunnies));
     bunnies = 5;
-    System.out.printf("[bunnies == %s]\n%s\n", bunnies, bunnyEars2(bunnies));
+    System.out.printf("[bunnies = %d]\n", bunnies);
+    System.out.println(bunnyEars2(bunnies));
     bunnies = 9;
-    System.out.printf("[bunnies == %s]\n%s\n", bunnies, bunnyEars2(bunnies));
+    System.out.printf("[bunnies = %d]\n", bunnies);
+    System.out.println(bunnyEars2(bunnies));
   }
 
   public static int bunnyEars2(int bunnies) {
     if (bunnies == 0) {
       return 0;
     }
-    StringBuilder s = new StringBuilder("");
-    s.toString();
     return (bunnies % 2 != 0 ? 2 : 3) + bunnyEars2(bunnies - 1);
   }
 }
